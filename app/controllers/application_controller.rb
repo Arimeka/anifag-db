d0f6e64c3022b7ea5d1b7fc644d477ed76aa2d68
Navi Arimeka <wrk.navi@gmail.com>
2013-05-26 23:15:56 +0400

    Add options to searching

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   private
-    def advanced_search(query,page)
+    def advanced_search(query, options={})
       str = query if query.present?
-      size = 10
-      from = page ? (page.to_i * size) - 1 : 0
+      fields = params[:search_in].keys if params[:search_in]
 
-      search = Tire.search ["#{Rails.env}-articles"], from: from, size: size do
-        query { string str } if str
-        sort { by :created_at, 'desc' }
+      search = Tire.search ["#{Rails.env}-articles"] do
+        query { string str, fields: fields } if str
+        sort { by options[:sort_by] || 'created_at', options[:sort_direction] || 'desc' }
+        filter :term, '_type' => options[:type] if options[:type]
+        page = (options[:page] || 1).to_i
+        search_size = options[:per] || 10
+        from (page -1) * search_size
+        size search_size
       end
 
       search.results
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,15 @@
+# coding: utf-8
 class StaticPagesController < ApplicationController
   expose(:articles) { Article.limit(3) }
-  expose(:results) { advanced_search(params[:query],params[:page]) }
+  expose(:results) { advanced_search(params[:query],page: params[:page], 
+                                     type: params[:type],sort_by: params[:sort_by],
+                                     sort_direction: params[:sort_direction]) }
 
   def home
   end
 
   def search
+    @sort_by = {'По релевантности' => '_score', 'По дате создания' => 'created_at'}
+    @sort_direction = {'По возрастанию' => 'asc', 'По убыванию' => 'desc'}
   end
 end
