306e5d879663f492c6ea10d344b73d606ae8099c
Navi Arimeka <wrk.navi@gmail.com>
2013-07-01 17:31:28 +0400

    More render to render God

--- spec/features/static_pages/home.html.haml_spec.rb
+++ spec/features/static_pages/home.html.haml_spec.rb
@@ -3,7 +3,7 @@ describe "the home page" do
   it "contains" do
     visit '/'
 
-    page.should have_content "Последние новости"
+    page.should have_content "Последние статьи"
     page.should have_content "Последние добавления в базу"
   end
 end
